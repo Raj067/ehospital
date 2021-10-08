c6239eeb39cb5a2ad7b0f01bb77e3f1f34b7da39
Rajabu-Mrisho <rajabumrisho69@gmail.com>
2021-10-08 20:11:16 +0300

    ---

--- reception/urls.py
+++ reception/urls.py
@@ -3,4 +3,9 @@ from .views import *
 
 reception_urls = [
     path('reception/', dashboard, name='dashboard'),
+    path('reception/noinsurance/', noinsurance, name='noinsurance'),
+    path('reception/insurance/', insurance, name='insurance'),
+    path('reception/patients/', patients, name='patients'),
+    path('reception/payments/', payments, name='payments'),
+    path('reception/complaints/', complaints, name='complaints'),
 ]
--- reception/views.py
+++ reception/views.py
@@ -5,3 +5,21 @@ from django.shortcuts import render
 
 def dashboard(request, *args, **kwargs):
     return render(request, 'reception/home.html', {})
+
+def insurance(request, *args, **kwargs):
+    return render(request, 'reception/insurance.html', {})
+
+
+def noinsurance(request, *args, **kwargs):
+    return render(request, 'reception/noinsurance.html', {})
+
+
+def patients(request, *args, **kwargs):
+    return render(request, 'reception/patients.html', {})
+
+def payments(request, *args, **kwargs):
+    return render(request, 'reception/payments.html', {})
+
+
+def complaints(request, *args, **kwargs):
+    return render(request, 'reception/complaints.html', {})
