2eff2c9351791f8005d97c2ed623ee5ff237f5c2
Rajabu-Mrisho <rajabumrisho69@gmail.com>
2021-10-09 00:34:23 +0300

    ---

--- reception/views.py
+++ reception/views.py
@@ -15,10 +15,26 @@ def noinsurance(request, *args, **kwargs):
 
 
 def patients(request, *args, **kwargs):
-    return render(request, 'reception/patients.html', {})
+    users = [
+        ['Rajabu Mrisho', 25, 'Male', 49, '23/56', '0679190720'],
+        ['Salehe Mrisho', 40, 'Male', 89, '90/45', '0679190720'],
+        ['Jamila Mrisho', 40, 'Female', 89, '90/45', '0679190720'],
+    ]
+    data={
+        'data': users
+    }
+    return render(request, 'reception/patients.html', data)
 
 def payments(request, *args, **kwargs):
-    return render(request, 'reception/payments.html', {})
+    users = [
+        ['1-10-2021', 'Rajabu Mrisho', 25, 'Male', '0679190720', 4500],
+        ['1-10-2021', 'Salehe Mrisho', 40, 'Male', '0679190720', 9000],
+        ['1-10-2021', 'Jamila Mrisho', 40, 'Female', '0679190720', 10000],
+    ]
+    data={
+        'data': users
+    }
+    return render(request, 'reception/payments.html', data)
 
 
 def complaints(request, *args, **kwargs):
