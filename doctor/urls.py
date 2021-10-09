6a4c972f0b2c331ff8e6b442b3ebf4ade3d0660c
Rajabu-Mrisho <rajabumrisho69@gmail.com>
2021-10-09 15:55:23 +0300

    ---

--- doctor/urls.py
+++ doctor/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from .views import *
 
 doctor_urls = [
-        path('doctor/', dashboard, name='dashboard'),
+    path('doctor/', dashboard, name='dashboard'),
+    path('doctor/admitted-patient/', admitted_patient, name='admitted_patient'),
+    path('doctor/outpatient/patient/',
+         outpatient_patient, name='outpatient_patient'),
+    path('doctor/report/', report, name='report'),
 ]
--- doctor/views.py
+++ doctor/views.py
@@ -2,8 +2,44 @@ from django.shortcuts import render
 
 # Create your views here.
 def dashboard(request, *args, **kwargs):
-    users = []
+    users = [
+        ['I0001', 'Rajabu Mrisho', 'Male', 43, 'Normal', '23/56'],
+        ['I0002', 'Salehe Mrisho', 'Male', 67, 'stable', '90/45'],
+        ['I0003', 'Jamila Mrisho', 'Female', 89, 'unstable', '90/45'],
+    ]
     data={
         'data': users
     }
     return render(request, 'doctor/home.html', data)
+
+
+def admitted_patient(request, *args, **kwargs):
+    users = [
+        ['I0001', 'Rajabu Mrisho', 'Male', 43, 'Normal', '23/56'],
+        ['I0002', 'Salehe Mrisho', 'Male', 67, 'stable', '90/45'],
+        ['I0003', 'Jamila Mrisho', 'Female', 89, 'unstable', '90/45'],
+    ]
+    data = {
+        'data': users
+    }
+    return render(request, 'doctor/admitted-patient.html', data)
+
+
+def outpatient_patient(request, *args, **kwargs):
+    users = [
+        ['I0001', 'Rajabu Mrisho', 'Male', 43, 'Normal', '23/56'],
+        ['I0002', 'Salehe Mrisho', 'Male', 67, 'stable', '90/45'],
+        ['I0003', 'Jamila Mrisho', 'Female', 89, 'unstable', '90/45'],
+    ]
+    data = {
+        'data': users
+    }
+    return render(request, 'doctor/outpatient-patient.html', data)
+
+
+def report(request, *args, **kwargs):
+    users = []
+    data = {
+        'data': users
+    }
+    return render(request, 'doctor/report.html', data)
