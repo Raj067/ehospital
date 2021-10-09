214a798ccaeb514567dca2e2c9a7b7a2f63122a2
Rajabu-Mrisho <rajabumrisho69@gmail.com>
2021-10-09 17:17:43 +0300

    ---

--- doctor/views.py
+++ doctor/views.py
@@ -43,3 +43,90 @@ def report(request, *args, **kwargs):
         'data': users
     }
     return render(request, 'doctor/report.html', data)
+
+def complaints(request, *args, **kwargs):
+    return render(request, 'doctor/compaints.html', {})
+
+
+def medication_request(request, *args, **kwargs):
+    return render(request, 'doctor/medication-request.html', {})
+
+
+def medication_completed(request, *args, **kwargs):
+    return render(request, 'doctor/medication-completed.html', {})
+
+
+def medication_new(request, *args, **kwargs):
+    return render(request, 'doctor/medication-new.html', {})
+
+
+def imaging_request(request, *args, **kwargs):
+    return render(request, 'doctor/imaging-request.html', {})
+
+
+def imaging_completed(request, *args, **kwargs):
+    return render(request, 'doctor/imaging-completed.html', {})
+
+
+def all_schedule(request, *args, **kwargs):
+    users = [
+        ['1-10-2021', '9:00AM - 12:00AM', 'Rajabu Mrisho',
+            'Male', 'Consultation', 'Normal'],
+        ['2-10-2021', '9:00AM - 12:00AM',
+            'Salehe Mrisho', 'Male', 'Imaging', 'stable'],
+        ['3-10-2021', '9:00AM - 12:00AM', 'Jamila Mrisho',
+            'Female', 'Imaging', 'unstable'],
+    ]
+    data = {
+        'data': users
+    }
+    return render(request, 'doctor/all_schedule.html', data)
+
+
+def today_schedule(request, *args, **kwargs):
+    users = [
+        ['1-10-2021', '9:00AM - 12:00AM', 'Rajabu Mrisho',
+            'Male', 'Consultation', 'Normal'],
+        ['2-10-2021', '9:00AM - 12:00AM',
+            'Salehe Mrisho', 'Male', 'Imaging', 'stable'],
+    ]
+    data = {
+        'data': users
+    }
+    return render(request, 'doctor/today_schedule.html', data)
+
+
+def add_schedule(request, *args, **kwargs):
+    users = [
+        ['I0001', 'Rajabu Mrisho', 'Male', 43, 'Normal', '23/56'],
+        ['I0002', 'Salehe Mrisho', 'Male', 67, 'stable', '90/45'],
+        ['I0003', 'Jamila Mrisho', 'Female', 89, 'unstable', '90/45'],
+    ]
+    data = {
+        'data': users
+    }
+    return render(request, 'doctor/add_schedule.html', data)
+
+
+def theater_schedule(request, *args, **kwargs):
+    users = [
+        ['I0001', 'Rajabu Mrisho', 'Male', 43, 'Normal', '23/56'],
+        ['I0002', 'Salehe Mrisho', 'Male', 67, 'stable', '90/45'],
+        ['I0003', 'Jamila Mrisho', 'Female', 89, 'unstable', '90/45'],
+    ]
+    data = {
+        'data': users
+    }
+    return render(request, 'doctor/theater_schedule.html', data)
+
+
+def add_theater_schedule(request, *args, **kwargs):
+    users = [
+        ['I0001', 'Rajabu Mrisho', 'Male', 43, 'Normal', '23/56'],
+        ['I0002', 'Salehe Mrisho', 'Male', 67, 'stable', '90/45'],
+        ['I0003', 'Jamila Mrisho', 'Female', 89, 'unstable', '90/45'],
+    ]
+    data = {
+        'data': users
+    }
+    return render(request, 'doctor/add_theater_schedule.html', data)
