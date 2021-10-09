410b089d8d64614db6a5c9db678ec04382702d36
Rajabu-Mrisho <rajabumrisho69@gmail.com>
2021-10-09 12:37:12 +0300

    ---

--- nurse/urls.py
+++ nurse/urls.py
@@ -3,4 +3,9 @@ from .views import *
 
 nurse_urls = [
     path('nurse/', dashboard, name='dashboard'),
+    path('nurse/all_schedule/', all_schedule, name='all_schedule'),
+    path('nurse/today_schedule/', today_schedule, name='today_schedule'),
+    path('nurse/add_schedule/', add_schedule, name='add_schedule'),
+    path('nurse/theater_schedule/', theater_schedule, name='theater_schedule'),
+    path('nurse/theater_schedule/', add_theater_schedule, name='theater_schedule'),
 ]
--- nurse/views.py
+++ nurse/views.py
@@ -3,11 +3,70 @@ from django.shortcuts import render
 # Create your views here.
 def dashboard(request, *args, **kwargs):
     users = [
-        ['Rajabu Mrisho', 25, 'Male', 49, '23/56', '0679190720'],
-        ['Salehe Mrisho', 40, 'Male', 89, '90/45', '0679190720'],
-        ['Jamila Mrisho', 40, 'Female', 89, '90/45', '0679190720'],
+        ['I0001', 'Rajabu Mrisho', 'Male', 43, 'Normal', '23/56'],
+        ['I0002', 'Salehe Mrisho', 'Male', 67, 'stable', '90/45'],
+        ['I0003', 'Jamila Mrisho', 'Female', 89, 'unstable', '90/45'],
     ]
     data={
         'data': users
     }
     return render(request, 'nurse/home.html', data)
+
+def all_schedule(request, *args, **kwargs):
+    users = [
+        ['I0001', 'Rajabu Mrisho', 'Male', 43, 'Normal', '23/56'],
+        ['I0002', 'Salehe Mrisho', 'Male', 67, 'stable', '90/45'],
+        ['I0003', 'Jamila Mrisho', 'Female', 89, 'unstable', '90/45'],
+    ]
+    data={
+        'data': users
+    }
+    return render(request, 'nurse/all_schedule.html', data)
+
+
+def today_schedule(request, *args, **kwargs):
+    users = [
+        ['I0001', 'Rajabu Mrisho', 'Male', 43, 'Normal', '23/56'],
+        ['I0002', 'Salehe Mrisho', 'Male', 67, 'stable', '90/45'],
+        ['I0003', 'Jamila Mrisho', 'Female', 89, 'unstable', '90/45'],
+    ]
+    data = {
+        'data': users
+    }
+    return render(request, 'nurse/today_schedule.html', data)
+
+
+def add_schedule(request, *args, **kwargs):
+    users = [
+        ['I0001', 'Rajabu Mrisho', 'Male', 43, 'Normal', '23/56'],
+        ['I0002', 'Salehe Mrisho', 'Male', 67, 'stable', '90/45'],
+        ['I0003', 'Jamila Mrisho', 'Female', 89, 'unstable', '90/45'],
+    ]
+    data = {
+        'data': users
+    }
+    return render(request, 'nurse/add_schedule.html', data)
+
+
+def theater_schedule(request, *args, **kwargs):
+    users = [
+        ['I0001', 'Rajabu Mrisho', 'Male', 43, 'Normal', '23/56'],
+        ['I0002', 'Salehe Mrisho', 'Male', 67, 'stable', '90/45'],
+        ['I0003', 'Jamila Mrisho', 'Female', 89, 'unstable', '90/45'],
+    ]
+    data = {
+        'data': users
+    }
+    return render(request, 'nurse/theater_schedule.html', data)
+
+
+def add_theater_schedule(request, *args, **kwargs):
+    users = [
+        ['I0001', 'Rajabu Mrisho', 'Male', 43, 'Normal', '23/56'],
+        ['I0002', 'Salehe Mrisho', 'Male', 67, 'stable', '90/45'],
+        ['I0003', 'Jamila Mrisho', 'Female', 89, 'unstable', '90/45'],
+    ]
+    data = {
+        'data': users
+    }
+    return render(request, 'nurse/add_theater_schedule.html', data)
